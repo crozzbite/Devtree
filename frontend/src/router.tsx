6fec405ad98fbf1e8acd0e13db00754dab1b00e2
crozzbite <jon.zavalag@gmail.com>
2025-02-23 21:46:50 -0600

    profile vista

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,6 +3,9 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
 import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
+import LinkTreeView from "./views/LinkTreeView";
+import ProfileView from "./views/ProfileView";
 
 
 // Archivo router.tsx
@@ -15,7 +18,10 @@ const AppRouter: React.FC = () => {
                     <Route path="/auth/register" element={<RegisterView />} />
                 </Route>
                 {/* Ruta para manejar páginas no encontradas */}
-                <Route path="*" element={<LoginView />} />
+                <Route path ="/admin" element={<AppLayout />}>
+                    <Route index = {true} element ={<LinkTreeView/>} />
+                    <Route path="profile" element ={<ProfileView/>} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')// para que jale la dependencia
+  ],
 }
 
