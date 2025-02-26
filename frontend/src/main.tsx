d5d5341a25003bbaec19d6c8c4fcbd9d1dbf1aaa
crozzbite <jon.zavalag@gmail.com>
2025-02-25 22:40:04 -0600

    safety para navegacion en perfil

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRouter from './router'
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
+    
   </StrictMode>,
 )
