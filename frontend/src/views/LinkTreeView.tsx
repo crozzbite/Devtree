ce2d6d3ffe2315dd74459a7ddcc9048b12673439
crozzbite <jon.zavalag@gmail.com>
2025-03-13 21:25:25 -0600

    acomodar links en front

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,15 +1,44 @@
 import { useState } from "react";
 import { social } from "../data/social";
 import DevTreeInput from "../components/DevTreeInput";
+import { isValidUrl } from "../utils";
+import { toast } from "sonner";
 
 export default function LinkTreeView() {
-    const [devTeeLinks] =  useState(social)
-  return <div className="space-y-5" >
-    {devTeeLinks.map((item) => (
+  const [devTeeLinks, setDevTreeLinks] = useState(social);
+
+  // agarraremos props y las pasaremos hacia el hijo
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTeeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link
+    );
+    setDevTreeLinks(updatedLinks);
+  };
+
+  const handleEnableLink = (SocialNetwork: string) => {
+    // cambiar el estado de enabled
+    const updatedLinks = devTeeLinks.map((link) => {
+      if (link.name === SocialNetwork) {
+        if (isValidUrl(link.url)) {
+          return { ...link, enabled: !link.enabled };
+        } else {
+          toast.error("URL no valida");
+        }
+      }
+      return link;
+    });
+    setDevTreeLinks(updatedLinks);
+  };
+  return (
+    <div className="space-y-5">
+      {devTeeLinks.map((item) => (
         <DevTreeInput
-            key={item.name}
-            item={item}
-            />
-        ))}
-  </div>;
+          key={item.name}
+          item={item}
+          handleUrlChange={handleUrlChange}
+          handleEnableLink={handleEnableLink}
+        />
+      ))}
+    </div>
+  );
 }
